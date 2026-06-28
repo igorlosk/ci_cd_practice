3a6a5116a504942df50d93d7e83e9a528d051d99
Igor Loskutnikov <igororder@gmail.com>
2026-06-28 19:19:36 +0300

    done

--- src/test/java/com/loskutnikov/ci_cd/api/PhoneControllerTest.java
+++ src/test/java/com/loskutnikov/ci_cd/api/PhoneControllerTest.java
@@ -18,6 +18,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @WebMvcTest(PhoneController.class)
 class PhoneControllerTest {
 
